e5ed27b0c86df30e422ca23b1275edde648b28ac
el-tecson <emmanuel.l.tecson@gmail.com>
2025-10-09 11:24:54 +0800

    feat: Install and setup vite

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'popup.html'),
+      },
+      output: {
+        entryFileNames: assetInfo => {
+          // keeps separate folders for background/content/popup
+          if (assetInfo.name.includes('background')) return 'background/[name].js'
+          if (assetInfo.name.includes('content')) return 'content/[name].js'
+          return 'assets/[name].js'
+        },
+      },
+    },
+  },
+})
