b70563d1cc77e0a0bd8af605b8d260c35dda70da
el-tecson <emmanuel.l.tecson@gmail.com>
2025-10-23 18:47:29 +0800

    feat: make default_popup's UI

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,7 +1,29 @@
+import Logo from "@/assets/lptimer-logo-wide.svg?react"
+import SettingsIcon from '@/assets/settings.svg?react'
+import ReloadIcon from '@/assets/reload.svg?react'
+import KoFiIcon from '@/assets/ko-fi.svg?react'
+import "@/styles/Popup.css"
+
 export default function Popup() {
     return (
-        <div>
-            <p>Hello World</p>
-        </div>
+        <section id="popup">
+            <div className="popup-container">
+                <Logo height="48" width="202" fill="#FFFFFF"/>
+                <div className="buttons-container">
+                    <button className="btn" id="openSettings">
+                        <SettingsIcon />
+                        <span className="btn-text">Settings</span>
+                    </button>
+                    <button className="btn" id="reloadTimer">
+                        <ReloadIcon />
+                        <span className="btn-text">Reload timer</span>
+                    </button>
+                    <button className="btn" id="donateBtn">
+                        <KoFiIcon />
+                        <span className="btn-text">Support me on Ko-Fi</span>
+                    </button>
+                </div>
+            </div>
+        </section>
     )
 }
