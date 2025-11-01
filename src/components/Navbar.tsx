230b7997cb201d70dbc217c834f6eb3d309031bd
el-tecson <emmanuel.l.tecson@gmail.com>
2025-11-01 23:54:25 +0800

    feat: Add active class to active tab

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import Logo from '@/assets/lptimer-logo-wide.svg?react'
 
 export default function Navbar() {
+    const location = useLocation();
+    const path = location.pathname;
+
     return (
         <nav className="navbar">
             <Link to="/">
@@ -9,13 +12,13 @@ export default function Navbar() {
             </Link>
             <div className="links">
                 <Link to="/about">
-                    <p className="link">About</p>
+                    <p className={`link ${path === "/about" || path === "/" ? "active" : ""}`}>About</p>
                 </Link>
                 <Link to="/how-to-use">
-                    <p className="link">How to Use</p>
+                    <p className={`link ${path === "/how-to-use" ? "active" : ""}`}>How to Use</p>
                 </Link>
                 <Link to="/settings">
-                    <p className="link">Settings</p>
+                    <p className={`link ${path === "/settings" ? "active" : ""}`}>Settings</p>
                 </Link>
             </div>
         </nav>
