ff409238668536445c37a727002094396d357c8e
el-tecson <emmanuel.l.tecson@gmail.com>
2025-11-07 13:52:46 +0800

    feat: Make custom Radio button component

---
+++ src/components/Forms/Radio.tsx
@@ -0,0 +1,54 @@
+import '@/styles/components/Forms/Radio.css';
+import { RadioGroup, Radio as HUIRadio } from '@headlessui/react';
+import { useState } from 'react';
+import changeBehaviorSettings from '@/utils/Settings/behavior';
+import CircleIcon from '@/assets/circle.svg?react';
+
+export default function Radio({
+    className = '',
+    initialState,
+    configName,
+    options,
+    label,
+    ...props
+}: {
+    className?: string;
+    initialState: string;
+    configName: string;
+    options: {
+        name: string;
+        optionName: string;
+    }[];
+    label: string;
+    [key: string]: any;
+}) {
+    const [selected, setSelected] = useState(initialState);
+
+    const handleChange = async (newValue: string) => {
+        setSelected(newValue);
+        await changeBehaviorSettings(configName, newValue);
+    };
+
+    return (
+        <RadioGroup
+            value={selected}
+            onChange={handleChange}
+            aria-label={label}
+            className="radio-group"
+            {...props}
+        >
+            {options.map((opt) => (
+                <HUIRadio
+                    key={opt.optionName}
+                    value={opt.optionName}
+                    className="radio"
+                >
+                    <div className="circle-icon-container">
+                        <CircleIcon className="circle-icon" />
+                    </div>
+                    <p className="input-text">{opt.name}</p>
+                </HUIRadio>
+            ))}
+        </RadioGroup>
+    )
+}
--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -1,7 +1,9 @@
 import Checkbox from "@/components/Forms/Checkbox";
 import Input from "@/components/Forms/Input";
+import Radio from "@/components/Forms/Radio";
 
 export {
     Checkbox,
-    Input
+    Input,
+    Radio,
 }
