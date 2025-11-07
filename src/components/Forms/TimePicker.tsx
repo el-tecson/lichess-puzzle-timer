a5133c5c3c55ad043c05125ac7a78eb5c3f80bc8
el-tecson <emmanuel.l.tecson@gmail.com>
2025-11-07 23:21:54 +0800

    feat: Make TimePicker component

---
+++ src/components/Forms/TimePicker.tsx
@@ -0,0 +1,136 @@
+import '@/styles/components/Forms/TimePicker.css';
+import { useState } from "react";
+import {
+    Dialog,
+    DialogPanel,
+    DialogBackdrop,
+    Listbox,
+    ListboxButton,
+    ListboxOptions,
+    ListboxOption,
+} from "@headlessui/react";
+import changeBehaviorSettings from "@/utils/Settings/behavior";
+
+export default function TimePicker({
+    initialState,
+    configName,
+    label,
+    ...props
+}: {
+    initialState: string;
+    configName: string;
+    label: string;
+    [key: string]: any;
+}) {
+    const [open, setOpen] = useState(false);
+
+    const [hInit = "00", mInit = "00", sInit = "00"]: string[] =
+        initialState.split(":");
+
+    const [hour, setHour] = useState(hInit);
+    const [minute, setMinute] = useState(mInit);
+    const [second, setSecond] = useState(sInit);
+
+    const hours = Array.from({ length: 24 }, (_, i) => String(i).padStart(2, "0"));
+    const minutes = Array.from({ length: 60 }, (_, i) => String(i).padStart(2, "0"));
+    const seconds = Array.from({ length: 60 }, (_, i) => String(i).padStart(2, "0"));
+
+    const handleChange = async ({
+        hour,
+        minute,
+        second,
+    }: {
+        hour: string;
+        minute: string;
+        second: string;
+    }) => {
+        setHour(hour);
+        setMinute(minute);
+        setSecond(second);
+        await changeBehaviorSettings(configName, `${hour}:${minute}:${second}`);
+    };
+
+    return (
+        <div className="time-picker-root number" {...props}>
+            <button
+                className="time-picker-trigger"
+                onClick={() => setOpen(true)}
+            >
+                {hour}:{minute}:{second}
+            </button>
+
+            <Dialog open={open} onClose={() => setOpen(false)} className="duration-dialog">
+                <DialogBackdrop className="duration-backdrop" />
+
+                <DialogPanel className="duration-panel">
+                    <p className="duration-panel-name">
+                        {label}
+                    </p>
+                    <div className="time-picker-dialog-inner number">
+
+                        {/* HOURS */}
+                        <div className="listbox-wrapper">
+                            <Listbox
+                                value={hour}
+                                onChange={(hour) =>
+                                    handleChange({ hour, minute, second })
+                                }
+                            >
+                                <ListboxButton className="listbox-button">{hour}</ListboxButton>
+                                <ListboxOptions className="listbox-options">
+                                    {hours.map((h) => (
+                                        <ListboxOption className="listbox-option" key={h} value={h}>
+                                            {h}
+                                        </ListboxOption>
+                                    ))}
+                                </ListboxOptions>
+                            </Listbox>
+                        </div>
+
+                        <span className="separator">:</span>
+
+                        {/* MINUTES */}
+                        <div className="listbox-wrapper">
+                            <Listbox
+                                value={minute}
+                                onChange={(minute) =>
+                                    handleChange({ hour, minute, second })
+                                }
+                            >
+                                <ListboxButton className="listbox-button">{minute}</ListboxButton>
+                                <ListboxOptions className="listbox-options">
+                                    {minutes.map((m) => (
+                                        <ListboxOption className="listbox-option" key={m} value={m}>
+                                            {m}
+                                        </ListboxOption>
+                                    ))}
+                                </ListboxOptions>
+                            </Listbox>
+                        </div>
+
+                        <span className="separator">:</span>
+
+                        {/* SECONDS */}
+                        <div className="listbox-wrapper">
+                            <Listbox
+                                value={second}
+                                onChange={(second) =>
+                                    handleChange({ hour, minute, second })
+                                }
+                            >
+                                <ListboxButton className="listbox-button">{second}</ListboxButton>
+                                <ListboxOptions className="listbox-options">
+                                    {seconds.map((s) => (
+                                        <ListboxOption className="listbox-option" key={s} value={s}>
+                                            {s}
+                                        </ListboxOption>
+                                    ))}
+                                </ListboxOptions>
+                            </Listbox>
+                        </div>
+                    </div>
+                </DialogPanel>
+            </Dialog>
+        </div>
+    );
+}
--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -1,9 +1,11 @@
 import Checkbox from "@/components/Forms/Checkbox";
 import Input from "@/components/Forms/Input";
 import Radio from "@/components/Forms/Radio";
+import TimePicker from "@/components/Forms/TimePicker";
 
 export {
     Checkbox,
     Input,
     Radio,
+    TimePicker,
 }
