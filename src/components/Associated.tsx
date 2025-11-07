387ccb52c3d3937e542c3e0263595d3e60825ebe
el-tecson <emmanuel.l.tecson@gmail.com>
2025-11-07 08:50:08 +0800

    feat: Make Associated component

---
+++ src/components/Associated.tsx
@@ -0,0 +1,16 @@
+import '@/styles/components/Associated.css';
+import type { ReactNode } from 'react';
+
+export default function Associated({
+    children,
+    ...props
+}: {
+    children?: ReactNode;
+    [key: string]: any;
+}) {
+    return (
+        <div className="associated" {...props}>
+            {children}
+        </div>
+    )
+}
