c5b5fe23369fbe531fcb688a699842980ded95ce
el-tecson <emmanuel.l.tecson@gmail.com>
2025-11-10 14:38:44 +0800

    feat: Make Customization-specific components

---
+++ src/components/Settings/CustomComponents/CustomizationComponents.tsx
@@ -0,0 +1,41 @@
+import { Listbox as CustomListbox, ColorPicker as CustomColorPicker } from "@/components/Forms";
+import type { ColorPickerProps, ListboxProps } from "@/types/components";
+import changeCustomizationSettings, { changeCustomsSettings } from "@/utils/Settings/customization";
+
+export function Listbox({
+    initialState,
+    configName,
+    storageFunction,
+    options,
+    label,
+    ...props
+}: Omit<ListboxProps, 'storageFunction'>) {
+    return (
+        <CustomListbox 
+            initialState={initialState}
+            configName={configName}
+            storageFunction={changeCustomizationSettings}
+            options={options}
+            label={label}
+            {...props}
+        />
+    )
+}
+
+export function ColorPicker({
+    initialState,
+    configName,
+    storageFunction,
+    label,
+    ...props
+}: Omit<ColorPickerProps, 'storageFunction'>) {
+    return (
+        <CustomColorPicker
+            initialState={initialState}
+            configName={configName}
+            storageFunction={changeCustomsSettings}
+            label={label}
+            {...props}
+        />
+    )
+}
