de5aa265f74da070b7adf9bda97f319a87743b79
el-tecson <emmanuel.l.tecson@gmail.com>
2025-11-11 21:30:23 +0800

    refactor: Transfer LocalWebsite code to main-local

--- src/main-local.tsx
+++ src/main-local.tsx
@@ -1,11 +1,26 @@
 import '@/background/loadDefaults';
 import '@/content/loadCustoms';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import SettingsPage from '@/pages/SettingsPage';
+import AboutPage from '@/pages/About';
+import HowToUsePage from '@/pages/HowToUse';
+import Navbar from '@/components/Navbar';
+import '@/styles/index.css';
+import '@/styles/local.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import LocalWebsite from '@/pages/LocalWebsite';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <LocalWebsite />
-    </StrictMode>,
+        <HashRouter>
+            <div id="background-overlay"></div>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<AboutPage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/how-to-use" element={<HowToUsePage />} />
+            </Routes>
+        </HashRouter>
+    </StrictMode>
 );
