e3c469c870dbee9020a8f9a862399d691c5d5d7b
el-tecson <emmanuel.l.tecson@gmail.com>
2025-11-27 17:08:00 +0800

    feat: Finish making the rest of the pages

--- src/pages/HowToUse.tsx
+++ src/pages/HowToUse.tsx
@@ -1,7 +1,27 @@
+import Step1Img from "@/assets/static/step-1.png";
+import Step2Img from "@/assets/static/step-2.png";
+import Step3Img from "@/assets/static/step-3.png";
+import "@/styles/About.css";
+import "@/styles/HowToUse.css";
+
 export default function HowToUsePage() {
     return (
         <>
             <h1 className="page-title">How to Use</h1>
+            <div className="page-body">
+                <section>
+                    <h2>Step 1: Go to Lichess website</h2>
+                    <img src={Step1Img} alt="step 1 image" />
+                </section>
+                <section>
+                    <h2>Step 2: Go to Puzzles</h2>
+                    <img src={Step2Img} alt="step 2 image" />
+                </section>
+                <section>
+                    <h2>Step 3: Start Solving</h2>
+                    <img src={Step3Img} alt="step 3 image" />
+                </section>
+            </div>
         </>
     );
 }
