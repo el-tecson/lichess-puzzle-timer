8aec951f420e518111c0aa2a3ac3f993d49d03a2
el-tecson <emmanuel.l.tecson@gmail.com>
2025-12-27 22:56:30 +0800

    fix: Only show puzzle timer on puzzle pages

--- src/utils/puzzleDetection.ts
+++ src/utils/puzzleDetection.ts
@@ -1,4 +1,4 @@
 export function isPuzzlePage() {
-    const domCheck = document.querySelector('.puzzle__board, .main-board') !== null;
+    const domCheck = document.querySelector('.puzzle__board') !== null;
     return domCheck;
 }
