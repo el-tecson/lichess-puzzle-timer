ef43b6bc933d9d2369e5662d64e489d78a5e3ff5
el-tecson <emmanuel.l.tecson@gmail.com>
2026-01-12 13:54:31 +0800

    feat: Make local website mobile-responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Logo from '@/assets/lptimer-logo-wide.svg?react';
+import {
+    Sidebar,
+    Menu,
+    MenuItem
+} from 'react-pro-sidebar';
 
 export default function Navbar() {
     const location = useLocation();
     const path = location.pathname;
+    const [toggled, setToggled] = useState(false);
 
     return (
         <nav className="navbar">
@@ -23,6 +30,43 @@ export default function Navbar() {
                     <p className={`link ${path === '/settings' ? 'active' : ''}`}>Settings</p>
                 </Link>
             </div>
+            <button
+                className="mobile-navbar-btn"
+                onClick={() => setToggled(!toggled)}
+                style={{
+                    display: "none",
+                }}
+            >
+                ☰
+            </button>
+
+            <Sidebar
+                className="mobile-navbar" 
+                toggled={toggled}
+                breakPoint="md"
+                style={{ display: "none" }}
+                onBackdropClick={() => setToggled(false)}
+            >
+                <Menu className="menu">
+                    <MenuItem className="menu-item">
+                        <Link to="/about">
+                            <p className={`link ${path === '/about' || path === '/' ? 'active' : ''}`}>
+                                About
+                            </p>
+                        </Link>
+                    </MenuItem>
+                    <MenuItem className="menu-item">
+                        <Link to="/how-to-use">
+                            <p className={`link ${path === '/how-to-use' ? 'active' : ''}`}>How to Use</p>
+                        </Link>
+                    </MenuItem>
+                    <MenuItem className="menu-item">
+                        <Link to="/settings">
+                            <p className={`link ${path === '/settings' ? 'active' : ''}`}>Settings</p>
+                        </Link>
+                    </MenuItem>
+                </Menu>
+            </Sidebar>
         </nav>
     );
 }
