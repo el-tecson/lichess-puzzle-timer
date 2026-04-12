5922f2a3939ddd86497e7d8be69410729321b224
el-tecson <emmanuel.l.tecson@gmail.com>
2026-04-12 16:42:38 +0800

    feat: Lead to firefox addon's reviews when using firefox

--- src/components/AnnouncementBar.tsx
+++ src/components/AnnouncementBar.tsx
@@ -1,10 +1,12 @@
 import '@/styles/AnnouncementBar.css';
 
 export default function AnnouncementBar() {
+    const reviewLink = getReviewLink();
+
     return (
         <div className="announcement-bar" id="announce">
             <span>Enjoying this extension? </span>
-            <a href="https://chromewebstore.google.com/detail/ifloeapglolidlgbfjnfidpnpnobddof/reviews" target="_blank" rel="noopener noreferrer">
+            <a href={reviewLink} target="_blank" rel="noopener noreferrer">
                 A quick review would mean a lot.
             </a>
             <button className="close-announcement" onClick={dismissAnnouncement}>✕</button>
@@ -16,3 +18,11 @@ function dismissAnnouncement() {
     const announcementBar = document.getElementById('announce');
     if (announcementBar) announcementBar.style.display = 'none';
 }
+
+const getReviewLink = () => {
+    if (typeof navigator === 'undefined') return '';
+
+    return navigator.userAgent.includes('Firefox')
+        ? 'https://addons.mozilla.org/en-US/firefox/addon/lichess-puzzle-timer/reviews/'
+        : 'https://chromewebstore.google.com/detail/ifloeapglolidlgbfjnfidpnpnobddof/reviews';
+};
